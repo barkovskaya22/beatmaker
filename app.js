16b6772417c7183ae85f61370c6fcd2c562f21b0
Ornella Barca <33401351+barkovskaya22@users.noreply.github.com>
2021-07-06 19:37:39 -0400

    Change in every file
    project

--- app.js
+++ app.js
@@ -15,33 +15,34 @@ class Drumkit {
         this.isPlaying = null;
         this.selects = document.querySelectorAll('select');
         this.muteBtns = document.querySelectorAll('.mute');
+        this.tempoSlider = document.querySelector('.tempo-slider');
     }
     activePad() {
         this.classList.toggle("active");
     }
 
-    repeat(){
+    repeat() {
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`)
         //loop over the pads
         activeBars.forEach(bar => {
             bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
             //check if pads are active
-            if(bar.classList.contains('active')){
+            if (bar.classList.contains('active')) {
                 //check each sound
-                if(bar.classList.contains('kick-pad')){
+                if (bar.classList.contains('kick-pad')) {
                     this.kickAudio.currentTime = 0;
                     this.kickAudio.play();
                 }
-                if(bar.classList.contains('snare-pad')){
+                if (bar.classList.contains('snare-pad')) {
                     this.snareAudio.currentTime = 0;
                     this.snareAudio.play();
                 }
-                if(bar.classList.contains('hihat-pad')){
+                if (bar.classList.contains('hihat-pad')) {
                     this.hihatAudio.currentTime = 0;
                     this.hihatAudio.play();
                 }
-                if(bar.classList.contains('others-pad')){
+                if (bar.classList.contains('others-pad')) {
                     this.othersAudio.currentTime = 0;
                     this.othersAudio.play();
                 }
@@ -53,7 +54,7 @@ class Drumkit {
     start() {
         const interval = (60 / this.bpm) * 1000;
         //check if it plays
-        if(!this.isPlaying) {
+        if (!this.isPlaying) {
             this.isPlaying = setInterval(() => {
                 this.repeat()
             }, interval);
@@ -64,8 +65,8 @@ class Drumkit {
         }
     }
     //button for play and stop changes
-    updateBtn(){
-        if(!this.isPlaying){
+    updateBtn() {
+        if (!this.isPlaying) {
             this.playBtn.innerText = "Stop";
             this.playBtn.classList.add("active");
         } else {
@@ -77,54 +78,67 @@ class Drumkit {
     changeSound(e) {
         const selectionName = e.target.name;
         const selectionValue = e.target.value;
-        switch(selectionName){
+        switch (selectionName) {
             case "kick-select":
-            this.kickAudio.src = selectionValue;
-            break;
+                this.kickAudio.src = selectionValue;
+                break;
             case "snare-select":
-            this.snareAudio.src = selectionValue;
-            break;
+                this.snareAudio.src = selectionValue;
+                break;
             case "hihat-select":
-            this.hihatAudio.src = selectionValue;
-            break;
+                this.hihatAudio.src = selectionValue;
+                break;
             case "others-select":
-            this.othersAudio.src = selectionValue;
-            break;
+                this.othersAudio.src = selectionValue;
+                break;
         }
     }
-    mute(e){
+    mute(e) {
         const muteIndex = e.target.getAttribute("data-track");
         e.target.classList.toggle("active");
-        if(e.target.classList.contains("active")){
-            switch(muteIndex){
+        if (e.target.classList.contains("active")) {
+            switch (muteIndex) {
                 case "0":
-                this.kickAudio.volume = 0;
-                break;
+                    this.kickAudio.volume = 0;
+                    break;
                 case "1":
-                this.snareAudio.volume = 0;
-                break;
+                    this.snareAudio.volume = 0;
+                    break;
                 case "2":
-                this.hihatAudio.volume = 0;
-                break;
+                    this.hihatAudio.volume = 0;
+                    break;
                 case "3":
-                this.othersAudio.volume = 0;
-                break;
+                    this.othersAudio.volume = 0;
+                    break;
             }
         } else {
-            switch(muteIndex) {
+            switch (muteIndex) {
                 case "0":
                     this.kickAudio.volume = 1;
                     break;
-                    case "1":
+                case "1":
                     this.snareAudio.volume = 1;
                     break;
-                    case "2":
+                case "2":
                     this.hihatAudio.volume = 1;
                     break;
-                    case "3":
+                case "3":
                     this.othersAudio.volume = 1;
                     break;
             }
+        }
+    }
+    changeTempo(e){
+        const tempoText = document.querySelector('.tempo-nr');
+        this.bpm = e.target.value;
+        tempoText.innerText = e.target.value;
+    }
+    updateTempo(){
+        clearInterval(this.isPlaying);
+        this.isPlaying = null;
+        const playBtn = document.querySelector('.play');
+        if (playBtn.classList.contains('active')){
+            this.start();
         }
     }
 }
@@ -136,24 +150,31 @@ const drumKit = new Drumkit();
 
 drumKit.pads.forEach(pad => {
     pad.addEventListener('click', drumKit.activePad);
-    pad.addEventListener('animationend', function(){
+    pad.addEventListener('animationend', function () {
         this.style.animation = "";
     });
 });
 
-drumKit.playBtn.addEventListener('click', function()  {
+drumKit.playBtn.addEventListener('click', function () {
     drumKit.updateBtn();
     drumKit.start();
 });
 
 drumKit.selects.forEach(select => {
-    select.addEventListener('change', function(e) {
+    select.addEventListener('change', function (e) {
         drumKit.changeSound(e);
     })
 })
 
 drumKit.muteBtns.forEach(btn => {
-    btn.addEventListener('click', function(e){
+    btn.addEventListener('click', function (e) {
         drumKit.mute(e);
     })
+})
+
+drumKit.tempoSlider.addEventListener('input', function(e){
+    drumKit.changeTempo(e);
+})
+drumKit.tempoSlider.addEventListener('change', function(e){
+    drumKit.updateTempo(e);
 })
